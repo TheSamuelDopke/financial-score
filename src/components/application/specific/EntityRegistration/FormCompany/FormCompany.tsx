986e21c9fa66db105cdb7fd1242c44cfcb2e45b1
TheSamuelDopke <samueloficial1@hotmail.com>
2025-12-22 16:27:15 -0300

    fix: update import paths for formatCNPJ and formatCPF functions

--- src/components/application/specific/EntityRegistration/FormCompany/FormCompany.tsx
+++ src/components/application/specific/EntityRegistration/FormCompany/FormCompany.tsx
@@ -13,7 +13,7 @@ import { Input } from "../../../reusable/Input/Input";
 import { Button } from "../../../reusable/Button/Button";
 
 import { toaster } from "@/components/ui/toaster";
-import { formatCNPJ } from "@/components/application/reusable/Scripts/validateCpfCnpj";
+import { formatCNPJ } from "../../../reusable/Scripts/validateCpfCnpj";
 
 export const FormCompany = () => {
   const {
--- src/components/application/specific/EntityRegistration/FormPeople/FormPeople.tsx
+++ src/components/application/specific/EntityRegistration/FormPeople/FormPeople.tsx
@@ -13,7 +13,7 @@ import { Button } from "../../../reusable/Button/Button";
 
 import { toaster } from "@/components/ui/toaster";
 
-import { formatCPF } from "@/components/application/reusable/Scripts/validateCpfCnpj";
+import { formatCPF } from "../../../reusable/Scripts/validateCpfCnpj";
 
 export const FormPeople = () => {
   const {
